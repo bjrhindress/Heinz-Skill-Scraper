6aeaca27cca276de55da1663c810415c78bf1d4f
bjrhindress <bjrhindress@gmail.com>
2019-02-09 22:56:12 -0500

    updated merge file

--- FileMerge_Updated10pm.py
+++ FileMerge_Updated10pm.py
@@ -12,16 +12,14 @@ df_cmu = pandas.read_csv('cmu_clean_data.csv')
 df_payscale = pandas.read_csv('payscale_clean_data.csv')
 df_indeed = pandas.read_csv('indeed_clean_data.csv')
 
-df1 = pandas.merge(df_indeed, df_payscale, left_on = "Keywords", right_on = "PopularSkills", how = "outer")
+df1 = pandas.merge(df_indeed, df_cmu, left_on = "Index", right_on = "Index", how = "outer")
 
 print(df1.head(10))
+print(df1)
 
+# df2 = pandas.merge(df_cmu, df_payscale, left_on = "Index", right_on = "Index", how = "outer")
+# print(df2.head(10))
 
-df2 = pandas.merge(df_cmu, df_payscale, left_on = "Course", right_on = "PopularSkills", how = "outer")
-print(df2.head(10))
-
-# creates .csv file
-myFile = open('project_merge.csv', 'w', newline='')
-with myFile:
-    writer = csv.writer(myFile)
-    writer.writerows(df2)
+#creates .csv file
+with open('project_merge.csv', 'w') as myFile:
+	df1.to_csv(myFile)
