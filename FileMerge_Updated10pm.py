d48645f7806ac9942914e393cb9b7e182122e594
bjrhindress <bjrhindress@gmail.com>
2019-02-09 23:26:07 -0500

    Final Draft Prototype version

--- FileMerge_Updated10pm.py
+++ FileMerge_Updated10pm.py
@@ -12,14 +12,12 @@ df_cmu = pandas.read_csv('cmu_clean_data.csv')
 df_payscale = pandas.read_csv('payscale_clean_data.csv')
 df_indeed = pandas.read_csv('indeed_clean_data.csv')
 
-df1 = pandas.merge(df_indeed, df_cmu, left_on = "Index", right_on = "Index", how = "outer")
+df1 = pandas.merge(df_indeed, df_cmu, left_on = "Merge", right_on = "Merge", how = "inner")
+df2 = pandas.merge(df1, df_payscale, left_on = "Merge", right_on = "Merge", how = "inner")
 
-print(df1.head(10))
-print(df1)
-
-# df2 = pandas.merge(df_cmu, df_payscale, left_on = "Index", right_on = "Index", how = "outer")
-# print(df2.head(10))
+# print(df1.head(10))
+# print(df1)
 
 #creates .csv file
 with open('project_merge.csv', 'w') as myFile:
-	df1.to_csv(myFile)
+	df2.to_csv(myFile)
