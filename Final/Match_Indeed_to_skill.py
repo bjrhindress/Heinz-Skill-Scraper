0232e3c4a7f3f79d464c2983df69022c2eb1d1f0
bjrhindress <bjrhindress@gmail.com>
2019-02-24 08:42:52 -0500

    master_controller pushed
    master_controller created
    match_to_indeed modularized

--- Final/Match_Indeed_to_skill.py
+++ Final/Match_Indeed_to_skill.py
@@ -4,72 +4,65 @@
 # In[148]:
 
 
-#read in list of skills as 
-import pandas as pd
-import re
-from bs4 import BeautifulSoup
-df_skills= pd.read_csv('payscale_clean_data.csv')
-#store the list of skills from payscale
-raw_skill_list=list(df_skills["PopularSkills"])
-#replace special characters with escapes, and split multiple skills separated by '/'
-skill_list=list()
-for i in range(0,len(raw_skill_list)):
-    skill=raw_skill_list[i]
-    skill=skill.replace(r'+',r'\+')
-    skill=skill.replace(r'.',r'\.')
-    skill=skill.replace(' / ','/')
-    skill=skill.replace(r')','')
-    skill=skill.split('/')
-    #break out parenthetical items into their own searches
-    skill_temp=list()
-    for a in skill:
-        b=a.split(r'(')
-        for c in b:
-            skill_temp.append(c)
-    skill=skill_temp
-    for x in skill:
-        skill_list.append(x)
-#pad everything with spaces at the start and end
-for i in range(0,len(skill_list)):
-    skill=skill_list[i]
-    skill=skill.center(len(skill)+2)
-    skill_list[i]=skill
-#remove OR since its a common word
-skill_list.remove(" OR ")
-
-
-# In[ ]:
-
-
-
-
-
-# In[131]:
+def get_skill_list():
+    #read in list of skills as 
+    import pandas as pd
+    import re
+    from bs4 import BeautifulSoup
+    df_skills= pd.read_csv('payscale_clean_data.csv')
+    #store the list of skills from payscale
+    raw_skill_list=list(df_skills["PopularSkills"])
+    #replace special characters with escapes, and split multiple skills separated by '/'
+    skill_list=list()
+    for i in range(0,len(raw_skill_list)):
+        skill=raw_skill_list[i]
+        skill=skill.replace(r'+',r'\+')
+        skill=skill.replace(r'.',r'\.')
+        skill=skill.replace(' / ','/')
+        skill=skill.replace(r')','')
+        skill=skill.split('/')
+        #break out parenthetical items into their own searches
+        skill_temp=list()
+        for a in skill:
+            b=a.split(r'(')
+            for c in b:
+                skill_temp.append(c)
+        skill=skill_temp
+        for x in skill:
+            skill_list.append(x)
+    #pad everything with spaces at the start and end
+    for i in range(0,len(skill_list)):
+        skill=skill_list[i]
+        skill=skill.center(len(skill)+2)
+        skill_list[i]=skill
+    #remove OR since its a common word
+    skill_list.remove(" OR ")
+    return(skill_list)
 
 
 #define function that scrapes one page at a time and returns a panda dataframe
-def scrape_page(page_num,job,city,state):
+def scrape_page(page_num,job,city,state,skill_list):
     import requests  # b_soup_1.py
     import re
     from bs4 import BeautifulSoup
     #spoof the user agent to make page scrapable
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-#define position search term (one word) and location
+    #define position search term (one word) and location
     #replace spaces in the user input job with plus
     position=job.replace(" ","+")
     location=city+"+"+state
     page_str=str(page_num)
-#url for search query
+    #url for search query
     url='https://www.indeed.com/jobs?q='+position+'&l='+location+'&start='+page_str
     search_page=requests.get(url,headers=headers)
     soup=BeautifulSoup(search_page.content,'html.parser')
     all_cards=soup.find_all(class_='jobsearch-SerpJobCard row result')
-#empty list to store job posting urls
+    #empty list to store job posting urls
     job_urls=[]
-#it seems to be easier to pull posting dates and locations from search page, so do that here
+    #it seems to be easier to pull posting dates and locations from search page, so do that here
     posting_dates=[]
     locations=[]
-#iterate through all_cards, extract job posting urls, posting dates, and locations
+    #iterate through all_cards, extract job posting urls, posting dates, and locations
     for card in all_cards:
     #urls
         job_id=card['data-jk']
@@ -85,43 +78,45 @@ def scrape_page(page_num,job,city,state):
     #locations
         job_location=card.find(class_='location').get_text()
         locations.append(job_location)
-#locations
-#lets make this dataframe
-#declare the empty lists that will become columns
+    
+   
+    #locations
+    #lets make this dataframe
+    #declare the empty lists that will become columns
     position_name=[]
     employer=[]
     full_description=[]
-#in the future, we can develop a list of keywords that we'll 
-  #search postings for, but for now, just return all capitalized
-   #words besides common words
+    #in the future, we can develop a list of keywords that we'll 
+    #search postings for, but for now, just return all capitalized
+    #words besides common words
     for job_url in job_urls:
-    #retrieve job page
+        #retrieve job page
         job_page=requests.get(job_url,headers=headers)
-    #make job page into BeautifulSoup object
+        #make job page into BeautifulSoup object
         job_soup=BeautifulSoup(job_page.content,'html.parser')
-    #retrieve the job title
+        #retrieve the job title
         job_position=job_soup.find(class_='icl-u-xs-mb--xs').get_text()
-    #retrieve company name from job rating line on page
+        #retrieve company name from job rating line on page
         job_rating_line=job_soup.find(class_='jobsearch-InlineCompanyRating')
-    #some jobs seem to not have a job rating line, so wrap this step in try
+        #some jobs seem to not have a job rating line, so wrap this step in try
         try:
             job_employer=job_rating_line.find(class_='icl-u-lg-mr--sm').get_text()
         except:
             job_employer=""
-    #retrieve full job description
+            #retrieve full job description
         job_descrip=job_soup.find(class_='jobsearch-JobComponent-description').get_text().upper()
-    #replace line breaks and common punctuation with spaces
+        #replace line breaks and common punctuation with spaces
         to_replace=['\n','\t','\.','\,','\(','\)','\;','\:']
         for x in to_replace:
             job_descrip.replace(x,' ')
             
-
-    #append all of the items created to the empty lists from last step
+        #append all of the items created to the empty lists from last step
         position_name.append(job_position)
         employer.append(job_employer)
         full_description.append(job_descrip)
-#for each skill in skill_list, search the job description to see if contains that skill
-#if it contains the skill, assign the value 1 to it and if not, assign 0
+        
+    #for each skill in skill_list, search the job description to see if contains that skill
+    #if it contains the skill, assign the value 1 to it and if not, assign 0
     #create blank dictionary that will store the 1-0 values
     skill_dict=dict()
     for skill in skill_list:
@@ -133,8 +128,7 @@ def scrape_page(page_num,job,city,state):
             else:
                 match_values.append(1)
         skill_dict[skill]=match_values
-                    
-#store results in dictionary, then data frame
+    #store results in dictionary, then data frame
     import pandas as pd
     result_dict={'Job Title':position_name,
                 'Employer':employer,
@@ -148,41 +142,22 @@ def scrape_page(page_num,job,city,state):
     return(result_frame)
 
 
-# In[ ]:
-
-
-
-
-
-# In[132]:
-
-
 #function that scrapes 5 pages
-def scrape_pages(user_input_job,user_input_city,user_input_state):
-    df=scrape_page(0,user_input_job,user_input_city,user_input_state)
+def scrape_pages(user_input_job,user_input_city,user_input_state,skill_list):
+    import pandas as pd
+    df=scrape_page(0,user_input_job,user_input_city,user_input_state,skill_list)
     for x in range(1,5):
-        temp=scrape_page(x,user_input_job,user_input_city,user_input_state)
+        temp=scrape_page(x,user_input_job,user_input_city,user_input_state,skill_list)
         df=pd.concat([df,temp])
     return(df)
 
 
-# In[135]:
-
-
-df=scrape_pages('analyst','pittsburgh','pa')
-
-
-# In[136]:
-
-
-
-
-
-# In[144]:
+#df=scrape_pages('analyst','pittsburgh','pa',skill_list)
 
 
 #function to return frame of skills listing the number of jobs that matched each skill
-def return_job_count(user_input_job,user_input_city,user_input_state):
+def return_job_count(skill_list,df):
+    import pandas as pd
     skill_sums=list()
     for skill in skill_list:
         skill_sums.append(sum(df[skill]))
@@ -192,22 +167,5 @@ def return_job_count(user_input_job,user_input_city,user_input_state):
     job_counts=job_counts[job_counts["Job_Matches"]>0]
     return(job_counts)
 
-# In[139]:
-
-df2 = return_job_count("Financial Analysts","Pittsburgh","PA")
-df2
-
-
-
-
-# In[145]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+#df2 = return_job_count("Financial Analysts","Pittsburgh","PA")
+#df2
--- Final/master_controller.py
+++ Final/master_controller.py
@@ -1,10 +1,35 @@
+# read in bls_data_frame module
 import bls_data_frame as b
-#read in the Match_Indeed_to_skill module
+# read in the Match_Indeed_to_skill module
 import Match_Indeed_to_skill as mi
+# read in the heinz_scraper module
+import heinz_scraper as hs
 
+# Get DF of BLS with Data
 df_bls = b.get_df_bls()
+
+# Get full list of BLS-tracked jobs
 job_list = b.get_job_list(df_bls)
 
+# Get full list of Payscale.com tracked skills 
 skill_list = mi.get_skill_list()
+
+# Get DF of number of jobs returned for a given job search from the BLS job_list
+job_df = mi.scrape_pages('Financial Analysts','Pittsburgh','PA',skill_list)
+
+# Get count of job skills for the searched job 
+job_skill_count = mi.return_job_count(skill_list,job_df)
+
+# Get skill_map
+skill_map = hs.get_skill_map(job_skill_count['Skill'])
+
+
+
+
+
+
+
+
+
 
 
